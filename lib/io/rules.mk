b2fca0b6a84d6fb9e2c1d97414c1d82a59e01a1c
Xavier Roumegue <xavier.roumegue@nxp.com>
2018-12-06 08:51:42 +0100

    lib: io: Prevent builtin printf if LINEBUFFER option is selected
    Signed-off-by: Xavier Roumegue <xavier.roumegue@nxp.com>

--- lib/io/rules.mk
+++ lib/io/rules.mk
@@ -9,4 +9,9 @@ MODULE_SRCS += \
    $(LOCAL_DIR)/console.c \
    $(LOCAL_DIR)/io.c \
 
+ifeq ($(WITH_DEBUG_LINEBUFFER), true)
+GLOBAL_DEFINES += WITH_DEBUG_LINEBUFFER=1
+GLOBAL_CFLAGS += -fno-builtin-printf
+endif
+
 include make/module.mk
